4b4dd4557537860f4a85890480a7a22eeca72228
Sayad Pervez <pervez2504@gmail.com>
2022-10-17 00:54:44 +0530

    rotating grad button

--- js/auto.js
+++ js/auto.js
@@ -1,5 +1,6 @@
 $(window).scroll(function(){
     $(".fadeup").css("opacity", 1 - $(window).scrollTop() / 500);
+    document.getElementById("registerHeroButton").style.background=`linear-gradient(${$(window).scrollTop()%360+45}deg,var(--neonBlue), var(--neonPink), var(--neonViolet))`;
 });
 
 $(document).ready(function() {
