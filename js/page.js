5e699dfbdd36df43b4b9347fbf6413b47aeb40f6
Sayad Pervez <pervez2504@gmail.com>
2022-10-17 23:26:01 +0530

    buttons added to all pages

--- js/page.js
+++ js/page.js
@@ -1,3 +1,8 @@
+$(window).scroll(function(){
+    $(".fadeup").css("opacity", 1 - $(window).scrollTop() / 500);
+    document.getElementById("registerHeroButton").style.background=`linear-gradient(${$(window).scrollTop()%360+45}deg,var(--neonBlue), var(--neonPink), var(--neonViolet))`;
+});
+
 $(document).ready(function() {
     setTimeout(function() {  myScrollTo('aboutdiv') }, 5000);
 });
